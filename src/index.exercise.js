259882ec7d752338e2ad0ccc3eb9ec704d84f579
Marco Beduschi <marco.beduschi@thoughtworks.com>
2021-03-01 08:12:51 -0300

    exercise 1.1 finished

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,48 +1,33 @@
-// 🐨 you'll need to import React and ReactDOM up here
+import '@reach/dialog/styles.css'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
-import {Logo} from './components/logo'
 import {Dialog} from '@reach/dialog'
-import '@reach/dialog/styles.css'
-import VisuallyHidden from '@reach/visually-hidden'
+import {Logo} from './components/logo'
 
-// 🐨 you'll also need to import the Logo component from './components/logo'
-
-// 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
-// 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
-const App = () => {
-  const openLoginDialog = () => setShowLoginDialog(true)
-  const closeLoginDialog = () => setShowLoginDialog(false)
-
-  const openRegisterDialog = () => setShowRegisterDialog(true)
-  const closeRegisterDialog = () => setShowRegisterDialog(false)
-
-  const [showLoginDialog, setShowLoginDialog] = React.useState(false)
-  const [showRegisterDialog, setShowRegisterDialog] = React.useState(false)
+function App() {
+  const [openModal, setOpenModal] = React.useState('none')
 
   return (
     <div>
-      <Logo height={80} width={80} />
+      <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
       <div>
-        <button onClick={openLoginDialog}>Login</button>
+        <button onClick={() => setOpenModal('login')}>Login</button>
       </div>
       <div>
-        <button onClick={openRegisterDialog}>Register</button>
+        <button onClick={() => setOpenModal('register')}>Register</button>
       </div>
-      <Dialog isOpen={showLoginDialog} onDismiss={closeLoginDialog}>
-        <button onClick={closeLoginDialog}>
-          <VisuallyHidden>Close</VisuallyHidden>
-          <span aria-hidden>x</span>
-        </button>
-        <p>Login Here!</p>
+      <Dialog aria-label="Login form" isOpen={openModal === 'login'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+        <h3>Login</h3>
       </Dialog>
-      <Dialog isOpen={showRegisterDialog} onDismiss={closeRegisterDialog}>
-        <button onClick={closeRegisterDialog}>
-          <VisuallyHidden>Close</VisuallyHidden>
-          <span aria-hidden>x</span>
-        </button>
-        <p>Register Here!</p>
+      <Dialog aria-label="Registration form" isOpen={openModal === 'register'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+        <h3>Register</h3>
       </Dialog>
     </div>
   )
